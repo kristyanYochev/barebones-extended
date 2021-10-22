cdd1cb0f7a62211dddad93eb764de04df67b9658
Kristyan Yochev <kris.yochev@gmail.com>
2021-10-22 16:34:27 +0100

    Added Environment class

---
+++ src/main/java/uk/ac/soton/ky5u21/barebones/Environment.java
@@ -0,0 +1,51 @@
+package uk.ac.soton.ky5u21.barebones;
+
+import java.util.HashMap;
+
+/**
+ * Instances of this class store the variable values during program execution
+ */
+public class Environment {
+  private final HashMap<String, Integer> variables = new HashMap<>();
+
+  /**
+   * Retrieves the value of a variable given the name. If the variable has not been encountered
+   * yet, its value is cleared and a 0 is returned.
+   * @param name The name of the variable
+   * @return The value of the variable
+   */
+  public int valueOf(String name) {
+    Integer value = variables.get(name);
+
+    if (value == null) {
+      clear(name);
+      return 0;
+    } else {
+      return value;
+    }
+  }
+
+  /**
+   * Sets the value of a variable to 0.
+   * @param name The name of the variable
+   */
+  public void clear(String name) {
+    variables.put(name, 0);
+  }
+
+  /**
+   * Adds 1 to the value of a variable and stores it back in itself.
+   * @param name The name of the variable
+   */
+  public void increment(String name) {
+    variables.put(name, valueOf(name) + 1);
+  }
+
+  /**
+   * Subtracts 1 from the value of the variable and stores it back in itself.
+   * @param name The name of the variable
+   */
+  public void decrement(String name) {
+    variables.put(name, valueOf(name) - 1);
+  }
+}
---
+++ src/test/java/uk/ac/soton/ky5u21/barebones/EnvironmentTest.java
@@ -0,0 +1,71 @@
+package uk.ac.soton.ky5u21.barebones;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EnvironmentTest {
+  @Test
+  public void theValueOfAVariableIsZeroByDefault() {
+    Environment environment = new Environment();
+
+    assertEquals(0, environment.valueOf("x"));
+    assertEquals(0, environment.valueOf("variable"));
+    assertEquals(0, environment.valueOf("another_variable"));
+  }
+
+  @Test
+  public void incrementingAdds1ToTheValueOfAVariable() {
+    Environment environment = new Environment();
+
+    assertEquals(0, environment.valueOf("x"));
+
+    environment.increment("x");
+    assertEquals(1, environment.valueOf("x"));
+
+    environment.increment("x");
+    assertEquals(2, environment.valueOf("x"));
+
+    environment.increment("x");
+    assertEquals(3, environment.valueOf("x"));
+  }
+
+  @Test
+  public void decrementingSubtracts1FromTheValueOfAVariable() {
+    Environment environment = new Environment();
+
+    assertEquals(0, environment.valueOf("x"));
+
+    environment.increment("x");
+    environment.increment("x");
+    environment.increment("x");
+
+    assertEquals(3, environment.valueOf("x"));
+
+    environment.decrement("x");
+    assertEquals(2, environment.valueOf("x"));
+
+    environment.decrement("x");
+    assertEquals(1, environment.valueOf("x"));
+
+    environment.decrement("x");
+    assertEquals(0, environment.valueOf("x"));
+  }
+
+  @Test
+  public void clearingAVariableResetsItsValueTo0() {
+    Environment environment = new Environment();
+
+    assertEquals(0, environment.valueOf("x"));
+
+    environment.increment("x");
+    environment.increment("x");
+    environment.increment("x");
+
+    assertEquals(3, environment.valueOf("x"));
+
+    environment.clear("x");
+
+    assertEquals(0, environment.valueOf("x"));
+  }
+}
